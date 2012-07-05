0e9128ff5ceb1d4b09e316fe4ce3ce6755e1ead6
Michael Fazio <michaelfazio@me.com>
2012-07-05 17:34:22 +0800

    Converted existing project to Maven project (Vaadin specified structure). Removed all IDE specific files. Added gitignore file. Upgraded demo project javascript sources (highcharts & jquery). Configured maven assembly for packaging as distributable .zip file.

---
+++ invient-charts/src/main/java/com/invient/vaadin/charts/Color.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2011 Invient (www.invient.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.invient.vaadin.charts;
+
+
+/**
+ * The Color interface represents RBG and RBGA colors.
+ * Do not confuse with {@link java.awt.Color} class. This is a simplified 
+ * version of {@link java.awt.Color} for the purpose of InvientCharts 
+ * 
+ * @author Invient
+ */
+@SuppressWarnings("serial")
+public interface Color extends Paint {
+
+    /**
+     * Represents RBG color value.
+     * 
+     * @author Invient
+     * 
+     */
+	public class RGB implements Color {
+        private int red;
+        private int green;
+        private int blue;
+
+        /**
+         * Creates an RGB color with the specified red, green, and blue values.
+         * The values must be in the range (0 - 255).
+         * 
+         * @param red
+         *            the red component in a color
+         * @param green
+         *            the green component in a color
+         * @param blue
+         *            the blue component in a color
+         */
+        public RGB(int red, int green, int blue) {
+            super();
+            String errorCompString = "";
+            boolean hasError = false;
+            if (red < 0 || red > 255) {
+                hasError = true;
+                errorCompString = " Red ";
+            }
+            if (green < 0 || green > 255) {
+                hasError = true;
+                errorCompString += " Green";
+            }
+            if (blue < 0 || blue > 255) {
+                hasError = true;
+                errorCompString += " Blue";
+            }
+            if (hasError) {
+                throw new IllegalArgumentException(
+                        "Color parameter outside of expected range:"
+                                + errorCompString);
+            }
+
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        /**
+         * 
+         * @return Returns the red component in the range (0-255).
+         */
+        public final int getRed() {
+            return red;
+        }
+
+        /**
+         * 
+         * @return Returns the green component in the range (0-255).
+         */
+        public final int getGreen() {
+            return green;
+        }
+
+        /**
+         * 
+         * @return Returns the blue component in the range (0-255).
+         */
+        public final int getBlue() {
+            return blue;
+        }
+
+        /**
+         * @return Returns string representation of this RBG.
+         */
+        @Override
+        public String getString() {
+            return new StringBuilder().append("rgb(").append(red).append(",")
+                    .append(green).append(",").append(blue).append(")")
+                    .toString();
+        }
+
+        /**
+         * @return Returns string representation of this RBG.
+         */
+        @Override
+        public String toString() {
+            return "RGB [red=" + red + ", green=" + green + ", blue=" + blue
+                    + "]";
+        }
+
+    }
+
+    /**
+     * Represents RGBA color value.
+     * @author Invient
+     *
+     */
+    public class RGBA extends RGB {
+        private float alpha = 1.0f;
+
+        /**
+         * Creates an RGBA color with the specified red, green, blue and alpha
+         * values. The red, green and blue values must be in the range (0 -
+         * 255). The alpha value must be in the range (0.0-1.0). The alpha value 
+         * deaults to 1.0
+         * 
+         * @param red
+         *            the red component in a color
+         * @param green
+         *            the green component in a color
+         * @param blue
+         *            the blue component in a color
+         * @param alpha
+         *            the alpha component in a color
+         */
+        public RGBA(int red, int green, int blue, float alpha) {
+            super(red, green, blue);
+            String errorCompString = "";
+            if (alpha < 0.0 || alpha > 1.0) {
+                errorCompString = " Alpha";
+                throw new IllegalArgumentException(
+                        "Color parameter outside of expected range: "
+                                + errorCompString);
+            }
+            this.alpha = alpha;
+        }
+
+        /**
+         * 
+         * @return Returns the alpha component in the range (0.0-1.0).
+         */
+        public final float getAlpha() {
+            return alpha;
+        }
+
+        /**
+         * @return Returns string representation of this RGBA
+         */
+        @Override
+        public String getString() {
+            return new StringBuilder().append("rgba(").append(getRed())
+                    .append(",").append(getGreen()).append(",")
+                    .append(getBlue()).append(",").append(alpha).append(")")
+                    .toString();
+        }
+
+        /**
+         * @return Returns string representation of this RGBA
+         */
+        @Override
+        public String toString() {
+            return "RGBA [alpha=" + alpha + ", red=" + getRed() + ", green="
+                    + getGreen() + ", blue=" + getBlue() + "]";
+        }
+
+    }
+
+}
---
+++ invient-charts/src/main/java/com/invient/vaadin/charts/Gradient.java
@@ -0,0 +1,399 @@
+/*
+ * Copyright 2011 Invient (www.invient.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.invient.vaadin.charts;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Gradient defines a way to fill a shape with a linear color gradient
+ * pattern.
+ * 
+ * @author Invient
+ */
+@SuppressWarnings("serial")
+public interface Gradient extends Paint {
+
+    public static enum Unit {
+        NUMBER("number", ""), PERCENT("percent", "%");
+        private String name;
+        private String symbol;
+
+        private Unit(String name, String symbol) {
+            this.name = name;
+            this.symbol = symbol;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String getSymbol() {
+            return this.symbol;
+        }
+    }
+
+    /**
+     * Returns the x-coordinate of a point at which linear gradient
+     *         starts.
+     * @return the x-coordinate of a point at which linear gradient
+     *         starts.
+     */
+    public int getxStart();
+
+    /**
+     * Returns the unit of x-coordinate of a point at which linear gradient starts.
+     * @return the unit of x-coordinate of a point at which linear gradient starts. 
+     */
+    public Unit getxStartUnit();
+
+    /**
+     * Returns the y-coordinate of a point at which linear gradient starts.
+     * @return the y-coordinate of a point at which linear gradient starts.
+     */
+    public int getyStart();
+
+    /**
+     * Returns the unit of y-coordinate of a point at which linear gradient starts.
+     * @return the unit of y-coordinate of a point at which linear gradient starts. 
+     */
+    public Unit getyStartUnit();
+
+    /**
+     * Returns the x-coordinate of a point at which linear gradient
+     *         ends.
+     * @return the x-coordinate of a point at which linear gradient
+     *         ends.
+     */
+    public int getxEnd();
+
+    /**
+     * Returns the unit of x-coordinate of a point at which linear gradient ends.
+     * @return the unit of x-coordinate of a point at which linear gradient ends. 
+     */
+    public Unit getxEndUnit();
+
+    /**
+     * Returns the x-coordinate of a point at which linear gradient ends.
+     * @return the x-coordinate of a point at which linear gradient ends.
+     */
+    public int getyEnd();
+
+    /**
+     * Returns the unit of y-coordinate of a point at which linear gradient ends.
+     * @return the unit of y-coordinate of a point at which linear gradient ends. 
+     */
+    public Unit getyEndUnit();
+
+    /**
+     * Returns a list of colorstops associated with this gradient.
+     * @return a list of colorstops associated with this gradient.
+     * @see ColorStop
+     * 
+     */
+    public List<? extends ColorStop> getColorStops();
+
+    /**
+     * Represents a stop-value and a color. The stop-value should be in range
+     * (0.0-1.0). The color of the gradient at each stop is the color specified
+     * for that stop. Between each such stop, the colors and the alpha component
+     * will be linearly interpolated over the RGBA.
+     * 
+     * @author Invient
+     * 
+     */
+    public interface ColorStop extends Serializable {
+
+        public double getStopAt();
+
+        public Unit getStopAtUnit();
+
+        public Color getColor();
+    }
+
+    /**
+     * Represents linear gradient where points of a linear gradient specify a
+     * line. For more details on gradient, refer to CSS 3 gradient
+     * documentation.
+     * 
+     * @author Invient
+     * 
+     */
+    public class LinearGradient implements Gradient {
+        private int xStart = 0;
+        private Unit xStartUnit = Unit.NUMBER;
+        private int yStart = 0;
+        private Unit yStartUnit = Unit.NUMBER;
+
+        private int xEnd = 0;
+        private Unit xEndUnit = Unit.NUMBER;
+        private int yEnd = 0;
+        private Unit yEndUnit = Unit.NUMBER;
+
+        private List<LinearColorStop> colorStops = new ArrayList<Gradient.LinearGradient.LinearColorStop>();
+
+        /**
+         * Creates a LinearGradient with the specified xStart, xEnd, yStart and
+         * yEnd values with default {@link Unit} value number.
+         * 
+         * @param xStart
+         *            the x-coordinate of a point at which linear gradient
+         *            starts.
+         * @param xEnd
+         *            the x-coordinate of a point at which linear gradient ends.
+         * @param yStart
+         *            the y-coordinate of a point at which linear gradient
+         *            starts.
+         * @param yEnd
+         *            the y-coordinate of a point at which linear gradient ends.
+         * @param colorStops
+         *            the list of colorstops for the linear gradient.
+         */
+        public LinearGradient(int xStart, int yStart, int xEnd, int yEnd,
+                List<LinearColorStop> colorStops) {
+            this(xStart, null, yStart, null, xEnd, null, yEnd, null, colorStops);
+        }
+
+        /**
+         * 
+         * @param xStart
+         *            the x-coordinate of a point at which linear gradient
+         *            starts.
+         * @param xStartUnit
+         *            the unit for the xStart value. It can have one of the two
+         *            values Unit.NUMBER or Unit.PERCENT. If it is null then the
+         *            default value is Unit.NUMBER.
+         * @param yStart
+         *            the y-coordinate of a point at which linear gradient
+         *            starts.
+         * @param yStartUnit
+         *            the unit for the yStart value. It can have one of the two
+         *            values Unit.NUMBER or Unit.PERCENT. If it is null then the
+         *            default value is Unit.NUMBER.
+         * @param xEnd
+         *            the x-coordinate of a point at which linear gradient ends.
+         * @param xEndUnit
+         *            the unit for the xEnd value. It can have one of the two
+         *            values Unit.NUMBER or Unit.PERCENT. If it is null then the
+         *            default value is Unit.NUMBER.
+         * @param yEnd
+         *            the y-coordinate of a point at which linear gradient ends.
+         * @param yEndUnit
+         *            the unit for the yEnd value. It can have one of the two
+         *            values Unit.NUMBER or Unit.PERCENT. If it is null then the
+         *            default value is Unit.NUMBER.
+         * @param colorStops
+         *            the list of colorstops for the linear gradient.
+         */
+        public LinearGradient(int xStart, Unit xStartUnit, int yStart,
+                Unit yStartUnit, int xEnd, Unit xEndUnit, int yEnd,
+                Unit yEndUnit, List<LinearColorStop> colorStops) {
+            super();
+            this.xStart = xStart;
+            if (xStartUnit != null) {
+                this.xStartUnit = xStartUnit;
+            }
+            this.yStart = yStart;
+            if (yStartUnit != null) {
+                this.yStartUnit = yStartUnit;
+            }
+            this.xEnd = xEnd;
+            if (xEndUnit != null) {
+                this.xEndUnit = xEndUnit;
+            }
+            this.yEnd = yEnd;
+            if (yEndUnit != null) {
+                this.yEndUnit = yEndUnit;
+            }
+            if (colorStops != null) {
+                this.colorStops = colorStops;
+            }
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see com.invient.vaadin.charts.Gradient#getxStart()
+         */
+        @Override
+        public int getxStart() {
+            return xStart;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see com.invient.vaadin.charts.Gradient#getxEnd()
+         */
+        @Override
+        public int getxEnd() {
+            return xEnd;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see com.invient.vaadin.charts.Gradient#getyStart()
+         */
+        @Override
+        public int getyStart() {
+            return yStart;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see com.invient.vaadin.charts.Gradient#getyEnd()
+         */
+        @Override
+        public int getyEnd() {
+            return yEnd;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see com.invient.vaadin.charts.Gradient#getxStartUnit()
+         */
+        @Override
+        public Unit getxStartUnit() {
+            return this.xStartUnit;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see com.invient.vaadin.charts.Gradient#getyStartUnit()
+         */
+        @Override
+        public Unit getyStartUnit() {
+            return this.yStartUnit;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see com.invient.vaadin.charts.Gradient#getxEndUnit()
+         */
+        @Override
+        public Unit getxEndUnit() {
+            return this.xEndUnit;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see com.invient.vaadin.charts.Gradient#getyEndUnit()
+         */
+        @Override
+        public Unit getyEndUnit() {
+            return this.yEndUnit;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see com.invient.vaadin.charts.Gradient#getColorStops()
+         */
+        @Override
+        public List<LinearColorStop> getColorStops() {
+            return colorStops;
+        }
+
+        /**
+         * Represents stop-value and color for the {@link LinearGradient}
+         * 
+         * @author Invient
+         * 
+         */
+        public static class LinearColorStop implements ColorStop {
+            private double stopAt;
+            private Unit stopAtUnit = Unit.NUMBER;
+            private Color color;
+
+            /**
+             * 
+             * @param stopAt
+             * @param color
+             */
+            public LinearColorStop(double stopAt, Color color) {
+                super();
+                this.stopAt = stopAt;
+                this.color = color;
+            }
+
+            /**
+             * 
+             * @param stopAt
+             * @param stopAtUnit
+             * @param color
+             */
+            public LinearColorStop(double stopAt, Unit stopAtUnit, Color color) {
+                super();
+                this.stopAt = stopAt;
+                if (stopAtUnit != null) {
+                    this.stopAtUnit = stopAtUnit;
+                }
+                this.color = color;
+            }
+
+            @Override
+            public double getStopAt() {
+                return stopAt;
+            }
+
+            @Override
+            public Unit getStopAtUnit() {
+                return stopAtUnit;
+            }
+
+            @Override
+            public Color getColor() {
+                return color;
+            }
+
+        }
+
+        /**
+         * @return Returns string representation of this LinearGradient
+         */
+        @Override
+        public String getString() {
+            StringBuilder sb = new StringBuilder();
+            // The prefix "JSOBJ:" indicates that the string is a JavaScript
+            // object
+            String x1 = "'" + this.xStart + this.xStartUnit.getSymbol() + "'";
+            String y1 = "'" + this.yStart + this.yStartUnit.getSymbol() + "'";
+            String x2 = "'" + this.xEnd + this.xEndUnit.getSymbol() + "'";
+            String y2 = "'" + this.yEnd + this.yEndUnit.getSymbol() + "'";
+
+            sb.append("JSOBJ:{");
+            sb.append(" linearGradient: [" + x1 + "," + y1 + "," + x2 + ","
+                    + y2 + "],");
+            sb.append(" stops: [");
+            int count = 0;
+            for (LinearColorStop colorStop : this.colorStops) {
+                if (colorStop.getColor() != null) {
+                    String stopAt = "'" + colorStop.getStopAt() + colorStop.getStopAtUnit().getSymbol() + "'";
+                    String stopColor =  "'" + colorStop.getColor().getString() + "'";
+                    if (count > 0) {
+                        sb.append(",");
+                    }
+                    sb.append("[" + stopAt + ", "
+                            + stopColor + "]");
+                    count++;
+                }
+            }
+            sb.append(" ]");
+            sb.append("}");
+            
+            return sb.toString();
+        }
+    }
+
+}
