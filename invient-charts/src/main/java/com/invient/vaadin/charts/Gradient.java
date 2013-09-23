ca25fbfea0f2d89c675ca96be224138743b2260c
Michael Fazio <michaelfazio@me.com>
2013-09-23 18:24:33 +0800

    Minor cleanup in to com.invient.vaadin.charts.Gradient

--- invient-charts/src/main/java/com/invient/vaadin/charts/Gradient.java
+++ invient-charts/src/main/java/com/invient/vaadin/charts/Gradient.java
@@ -22,14 +22,16 @@ import java.util.List;
 /**
  * The Gradient defines a way to fill a shape with a linear color gradient
  * pattern.
- * 
+ *
  * @author Invient
  */
 @SuppressWarnings("serial")
 public interface Gradient extends Paint {
 
     public static enum Unit {
+
         NUMBER("number", ""), PERCENT("percent", "%");
+
         private String name;
         private String symbol;
 
@@ -45,65 +47,53 @@ public interface Gradient extends Paint {
         public String getSymbol() {
             return this.symbol;
         }
+
     }
 
     /**
-     * Returns the x-coordinate of a point at which linear gradient
-     *         starts.
-     * @return the x-coordinate of a point at which linear gradient
+     * @return The x-coordinate of a point at which linear gradient
      *         starts.
      */
     public int getxStart();
 
     /**
-     * Returns the unit of x-coordinate of a point at which linear gradient starts.
-     * @return the unit of x-coordinate of a point at which linear gradient starts. 
+     * @return The unit of x-coordinate of a point at which linear gradient starts.
      */
     public Unit getxStartUnit();
 
     /**
-     * Returns the y-coordinate of a point at which linear gradient starts.
-     * @return the y-coordinate of a point at which linear gradient starts.
+     * @return The y-coordinate of a point at which linear gradient starts.
      */
     public int getyStart();
 
     /**
-     * Returns the unit of y-coordinate of a point at which linear gradient starts.
-     * @return the unit of y-coordinate of a point at which linear gradient starts. 
+     * @return The unit of y-coordinate of a point at which linear gradient starts.
      */
     public Unit getyStartUnit();
 
     /**
-     * Returns the x-coordinate of a point at which linear gradient
-     *         ends.
-     * @return the x-coordinate of a point at which linear gradient
-     *         ends.
+     * @return The x-coordinate of a point at which linear gradient ends.
      */
     public int getxEnd();
 
     /**
-     * Returns the unit of x-coordinate of a point at which linear gradient ends.
-     * @return the unit of x-coordinate of a point at which linear gradient ends. 
+     * @return The unit of x-coordinate of a point at which linear gradient ends.
      */
     public Unit getxEndUnit();
 
     /**
-     * Returns the x-coordinate of a point at which linear gradient ends.
-     * @return the x-coordinate of a point at which linear gradient ends.
+     * @return The x-coordinate of a point at which linear gradient ends.
      */
     public int getyEnd();
 
     /**
-     * Returns the unit of y-coordinate of a point at which linear gradient ends.
-     * @return the unit of y-coordinate of a point at which linear gradient ends. 
+     * @return The unit of y-coordinate of a point at which linear gradient ends.
      */
     public Unit getyEndUnit();
 
     /**
-     * Returns a list of colorstops associated with this gradient.
-     * @return a list of colorstops associated with this gradient.
+     * @return A list of {@link ColorStop} instances associated with this gradient.
      * @see ColorStop
-     * 
      */
     public List<? extends ColorStop> getColorStops();
 
@@ -112,9 +102,8 @@ public interface Gradient extends Paint {
      * (0.0-1.0). The color of the gradient at each stop is the color specified
      * for that stop. Between each such stop, the colors and the alpha component
      * will be linearly interpolated over the RGBA.
-     * 
+     *
      * @author Invient
-     * 
      */
     public interface ColorStop extends Serializable {
 
@@ -129,9 +118,8 @@ public interface Gradient extends Paint {
      * Represents linear gradient where points of a linear gradient specify a
      * line. For more details on gradient, refer to CSS 3 gradient
      * documentation.
-     * 
+     *
      * @author Invient
-     * 
      */
     public class LinearGradient implements Gradient {
         private int xStart = 0;
@@ -149,60 +137,45 @@ public interface Gradient extends Paint {
         /**
          * Creates a LinearGradient with the specified xStart, xEnd, yStart and
          * yEnd values with default {@link Unit} value number.
-         * 
-         * @param xStart
-         *            the x-coordinate of a point at which linear gradient
-         *            starts.
-         * @param xEnd
-         *            the x-coordinate of a point at which linear gradient ends.
-         * @param yStart
-         *            the y-coordinate of a point at which linear gradient
-         *            starts.
-         * @param yEnd
-         *            the y-coordinate of a point at which linear gradient ends.
-         * @param colorStops
-         *            the list of colorstops for the linear gradient.
+         *
+         * @param xStart     The x-coordinate of a point at which linear gradient
+         *                   starts.
+         * @param xEnd       The x-coordinate of a point at which linear gradient ends.
+         * @param yStart     The y-coordinate of a point at which linear gradient
+         *                   starts.
+         * @param yEnd       The y-coordinate of a point at which linear gradient ends.
+         * @param colorStops The list of colorstops for the linear gradient.
          */
         public LinearGradient(int xStart, int yStart, int xEnd, int yEnd,
-                List<LinearColorStop> colorStops) {
+                              List<LinearColorStop> colorStops) {
             this(xStart, null, yStart, null, xEnd, null, yEnd, null, colorStops);
         }
 
         /**
-         * 
-         * @param xStart
-         *            the x-coordinate of a point at which linear gradient
-         *            starts.
-         * @param xStartUnit
-         *            the unit for the xStart value. It can have one of the two
-         *            values Unit.NUMBER or Unit.PERCENT. If it is null then the
-         *            default value is Unit.NUMBER.
-         * @param yStart
-         *            the y-coordinate of a point at which linear gradient
-         *            starts.
-         * @param yStartUnit
-         *            the unit for the yStart value. It can have one of the two
-         *            values Unit.NUMBER or Unit.PERCENT. If it is null then the
-         *            default value is Unit.NUMBER.
-         * @param xEnd
-         *            the x-coordinate of a point at which linear gradient ends.
-         * @param xEndUnit
-         *            the unit for the xEnd value. It can have one of the two
-         *            values Unit.NUMBER or Unit.PERCENT. If it is null then the
-         *            default value is Unit.NUMBER.
-         * @param yEnd
-         *            the y-coordinate of a point at which linear gradient ends.
-         * @param yEndUnit
-         *            the unit for the yEnd value. It can have one of the two
-         *            values Unit.NUMBER or Unit.PERCENT. If it is null then the
-         *            default value is Unit.NUMBER.
-         * @param colorStops
-         *            the list of colorstops for the linear gradient.
+         * @param xStart     The x-coordinate of a point at which linear gradient
+         *                   starts.
+         * @param xStartUnit The unit for the xStart value. It can have one of the two
+         *                   values Unit.NUMBER or Unit.PERCENT. If it is null then the
+         *                   default value is Unit.NUMBER.
+         * @param yStart     The y-coordinate of a point at which linear gradient
+         *                   starts.
+         * @param yStartUnit The unit for the yStart value. It can have one of the two
+         *                   values Unit.NUMBER or Unit.PERCENT. If it is null then the
+         *                   default value is Unit.NUMBER.
+         * @param xEnd       the x-coordinate of a point at which linear gradient ends.
+         * @param xEndUnit   The unit for the xEnd value. It can have one of the two
+         *                   values Unit.NUMBER or Unit.PERCENT. If it is null then the
+         *                   default value is Unit.NUMBER.
+         * @param yEnd       The y-coordinate of a point at which linear gradient ends.
+         * @param yEndUnit   The unit for the yEnd value. It can have one of the two
+         *                   values Unit.NUMBER or Unit.PERCENT. If it is null then the
+         *                   default value is Unit.NUMBER.
+         * @param colorStops The list of colorstops for the linear gradient.
          */
         public LinearGradient(int xStart, Unit xStartUnit, int yStart,
-                Unit yStartUnit, int xEnd, Unit xEndUnit, int yEnd,
-                Unit yEndUnit, List<LinearColorStop> colorStops) {
-            super();
+                              Unit yStartUnit, int xEnd, Unit xEndUnit, int yEnd,
+                              Unit yEndUnit, List<LinearColorStop> colorStops) {
+
             this.xStart = xStart;
             if (xStartUnit != null) {
                 this.xStartUnit = xStartUnit;
@@ -222,6 +195,7 @@ public interface Gradient extends Paint {
             if (colorStops != null) {
                 this.colorStops = colorStops;
             }
+
         }
 
         /*
@@ -307,9 +281,8 @@ public interface Gradient extends Paint {
 
         /**
          * Represents stop-value and color for the {@link LinearGradient}
-         * 
+         *
          * @author Invient
-         * 
          */
         public static class LinearColorStop implements ColorStop {
             private double stopAt;
@@ -317,24 +290,20 @@ public interface Gradient extends Paint {
             private Color color;
 
             /**
-             * 
-             * @param stopAt
-             * @param color
+             * @param stopAt The stop-at value.
+             * @param color The color.
              */
             public LinearColorStop(double stopAt, Color color) {
-                super();
                 this.stopAt = stopAt;
                 this.color = color;
             }
 
             /**
-             * 
-             * @param stopAt
-             * @param stopAtUnit
-             * @param color
+             * @param stopAt The stop-at value.
+             * @param stopAtUnit The stop-at unit value.
+             * @param color The color.
              */
             public LinearColorStop(double stopAt, Unit stopAtUnit, Color color) {
-                super();
                 this.stopAt = stopAt;
                 if (stopAtUnit != null) {
                     this.stopAtUnit = stopAtUnit;
@@ -365,33 +334,30 @@ public interface Gradient extends Paint {
         @Override
         public String getString() {
             StringBuilder sb = new StringBuilder();
-            // The prefix "JSOBJ:" indicates that the string is a JavaScript
-            // object
+            // The prefix "JSOBJ:" indicates that the string is a JavaScript object
             String x1 = "'" + this.xStart + this.xStartUnit.getSymbol() + "'";
             String y1 = "'" + this.yStart + this.yStartUnit.getSymbol() + "'";
             String x2 = "'" + this.xEnd + this.xEndUnit.getSymbol() + "'";
             String y2 = "'" + this.yEnd + this.yEndUnit.getSymbol() + "'";
 
             sb.append("JSOBJ:{");
-            sb.append(" linearGradient: [" + x1 + "," + y1 + "," + x2 + ","
-                    + y2 + "],");
+            sb.append(" linearGradient: [").append(x1).append(",").append(y1).append(",").append(x2).append(",").append(y2).append("],");
             sb.append(" stops: [");
             int count = 0;
             for (LinearColorStop colorStop : this.colorStops) {
                 if (colorStop.getColor() != null) {
                     String stopAt = "'" + colorStop.getStopAt() + colorStop.getStopAtUnit().getSymbol() + "'";
-                    String stopColor =  "'" + colorStop.getColor().getString() + "'";
+                    String stopColor = "'" + colorStop.getColor().getString() + "'";
                     if (count > 0) {
                         sb.append(",");
                     }
-                    sb.append("[" + stopAt + ", "
-                            + stopColor + "]");
+                    sb.append("[").append(stopAt).append(", ").append(stopColor).append("]");
                     count++;
                 }
             }
             sb.append(" ]");
             sb.append("}");
-            
+
             return sb.toString();
         }
     }
